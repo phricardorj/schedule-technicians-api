8471e8d9dba15704c9a0f6913ebb098a90049bc2
Pedro Ricardo <brpedroricardo@gmail.com>
2023-03-30 03:19:14 -0300

    feat: Javadoc-style comment for CNPJ and CPF validation annotation

--- src/main/java/br/com/phricardo/schedulingtechnicians/validation/cnpj/CNPJ.java
+++ src/main/java/br/com/phricardo/schedulingtechnicians/validation/cnpj/CNPJ.java
@@ -8,6 +8,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Validates that the annotated string is a valid Brazilian CNPJ (Cadastro Nacional de Pessoa Jurídica).
+ *
+ * The CNPJ must be an 18 character string, with separators, because the dots, diagonal slash and hyphen are mandatory.
+ *
+ * Author: Pedro Henrique Ricardo (@phricardorj | phricardo.com.br)
+ * Created: March 30, 2023
+ *
+ * @since 1.0
+ */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = CnpjValidator.class)
--- src/main/java/br/com/phricardo/schedulingtechnicians/validation/cpf/CPF.java
+++ src/main/java/br/com/phricardo/schedulingtechnicians/validation/cpf/CPF.java
@@ -8,6 +8,16 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Validates that the annotated string is a valid Brazilian CPF (Cadastro de Pessoas Físicas).
+ *
+ * The CPF must be a 14 character string, with separators, because the periods and the hyphen are mandatory.
+ *
+ * Author: Pedro Henrique Ricardo (@phricardorj | phricardo.com.br)
+ * Created: March 30, 2023
+ *
+ * @since 1.0
+ * */
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = CpfValidator.class)
