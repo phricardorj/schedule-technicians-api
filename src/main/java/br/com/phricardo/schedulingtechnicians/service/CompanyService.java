93a0fd4996d66e0d9e8b2261aac6ab0c52d08bf4
Pedro Ricardo <brpedroricardo@gmail.com>
2023-04-02 05:04:13 -0300

    feat: add JPA show SQL to application.yml and format SQL

--- src/main/java/br/com/phricardo/schedulingtechnicians/service/CompanyService.java
+++ src/main/java/br/com/phricardo/schedulingtechnicians/service/CompanyService.java
@@ -16,9 +16,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import static java.util.Objects.nonNull;
 import static org.springframework.http.HttpStatus.OK;
 
